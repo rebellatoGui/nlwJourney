614ee1a4a19572d68e79dd82e2165b6f02c9d385
Gui <guilhermerebellato7@gmail.com>
2024-07-09 20:21:13 -0300

    feat: Implementing links_repository with test

--- src/models/repositories/emails_to_invite_repository.py
+++ src/models/repositories/emails_to_invite_repository.py
@@ -28,8 +28,8 @@ class EmailsToInviteRepository:
         cursor.execute(
             '''SELECT * FROM emails_to_invite WHERE trip_id = ?''', (trip_id,)
         )
-        trip = cursor.fetchall()  
-        return trip
+        emails = cursor.fetchall()  
+        return emails
     
 
     
--- src/models/repositories/emails_to_invite_repository_test.py
+++ src/models/repositories/emails_to_invite_repository_test.py
@@ -11,7 +11,6 @@ trip_id = str(uuid.uuid4())
 def test_registry_email():
     conn = db_connection_handler.get_connection()   
     emails_to_invite_repository = EmailsToInviteRepository(conn)
-    
     email_trips_infos = {
         "id": str(uuid.uuid4()),
         "trip_id": trip_id,
